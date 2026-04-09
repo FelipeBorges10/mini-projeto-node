c612f90a155a4f6ccf448d4b102a9e4033293d68
CAL Felipe <contatofelipeborges150324@gmail.com>
2026-04-09 09:10:07 -0300

    Fix user routes middleware and remove duplicate routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,14 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
+import { authMiddleare } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
-router.get("/", userController.getAll);
+router.get("/",authMiddleare, userController.getAll);
+
 router.get("/:id", userController.getById);
 router.post("/", userController.create);
 router.put("/:id", userController.update);
 router.delete("/:id", userController.remove);
 
 export default router;
-
-import {authMiddleware } from"../middlewares/authMiddlewares.js";
-
-router.get("/", authMiddleware,
-    userController.getAll);
